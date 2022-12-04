ec8e5b28a797d0f5a19ece0c1837b0106d8e67f7
Asil <asilefeakyol@gmail.com>
2022-12-04 18:04:15 +0300

    styling

--- pages/profile/Profile.tsx
+++ pages/profile/Profile.tsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect } from 'react';
 import { useColorScheme } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
-import { Button, View, Text, StatusBar } from 'native-base';
+import { Button, View, Text, StatusBar, Heading } from 'native-base';
 import auth from '@react-native-firebase/auth';
 import { UserContext } from '../../contexts/UserContext';
 
@@ -26,10 +26,14 @@ const Profile = () => {
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <View>
-        <Text>Profile</Text>
-        <Text>{user.email}</Text>
-        <Button onPress={handleLogout}>logout</Button>
+      <View my="2" mx="4">
+        <Heading textAlign="center">Profil</Heading>
+        <Text textAlign="center" my="2">
+          Email: {user.email}
+        </Text>
+        <Button colorScheme="red" onPress={handleLogout} my="2">
+          Çıkış Yap
+        </Button>
       </View>
     </SafeAreaView>
   );
